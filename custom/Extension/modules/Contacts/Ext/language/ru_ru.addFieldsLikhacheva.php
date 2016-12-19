dc538780f56bf03db55e1c73e132d977b1d90506
PiterIrene <i_lihacheva@mail.ru>
2016-12-19 11:05:56 +0300

    SuiteCRM_test: add 5 fields

---
+++ custom/Extension/modules/Contacts/Ext/language/ru_ru.addFieldsLikhacheva.php
@@ -0,0 +1,10 @@
+<?php
+$mod_strings = array (
+  'LBL_CATEGORY' => 'Категория',
+  'LBL_POSITION' => 'Должность',
+  'LBL_PAY' => 'Зарплата',
+  'LBL_SOCIAL_NET' => 'Социальная сеть',
+  'LBL_VIP' => 'VIP',
+  
+);
+?>
